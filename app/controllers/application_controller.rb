f156dcbb1c2b176e93dc618a6997a1275869f7b4
yatsun <yatsunlam25@gmail.com>
2021-06-01 01:12:52 -0400

    commit tree 1b65b442d743e694f5f0d77895c1a1064df02709

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,31 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    # @user = User.find_by(email: params[:email])
+    @user = User.find_by(username: params[:username])
+
+    # if @user.email == params[:email] && @user.password == params[:password]
+    if @user && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
 
   end
 
   get '/account' do
-
+    @user = User.find_by_id(session[:user_id])
+    if @user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to "/"
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+
+    def Helpers.current_user(me)
+        @user = User.find(me[:user_id])
+    end
+
+    def self.is_logged_in?(me) # method is asking "is logged in?"
+         !!me[:user_id] # !! sets this function to boolean
+    end
 end
